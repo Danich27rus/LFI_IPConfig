48f92bfec0b746e90ed53e8df4db8e3bdabd5a2c
Григорьев Даниил Андреевич <grigorev@rim.local>
2023-05-23 18:03:56 +0700

    initial

---
+++ IpAddressControlDemo/IpAddressControl/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace IpAddressControl
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged<T>(ref T property, T newValue, [CallerMemberName] string propertyName = "")
+        {
+            property = newValue;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ IpAddressControlDemo/IpAddressControlDemo/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using IpAddressControl;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpAddressControlDemo
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private string heading;
+
+        public string Heading
+        {
+            get => heading; 
+            set => OnPropertyChanged(ref heading, value);
+        }
+
+        private IpAddressViewModel ipCtrlVm;
+
+        public IpAddressViewModel IpAddressControlVM
+        {
+            get => ipCtrlVm; 
+            set => OnPropertyChanged(ref ipCtrlVm, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            IpAddressControlVM = new IpAddressViewModel();
+            Heading = "Use invalid characters for validation." + Environment.NewLine 
+                + "Use . to move to next box automatically." + Environment.NewLine 
+                + "Use out of range values.";
+        }
+    }
+}
